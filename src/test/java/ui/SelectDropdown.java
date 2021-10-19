a466c00afbaab87bfb103ccb899498b3b3c71334
Abdullah Al Noman <noman@mcc.com.bd>
2021-10-19 16:04:56 +0600

    Add in the first Part, getFirstSelectedOption();

--- src/test/java/ui/SelectDropdown.java
+++ src/test/java/ui/SelectDropdown.java
@@ -1,6 +1,6 @@
 package ui;
 
-import java.util.List;
+//import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -21,10 +21,42 @@ public class SelectDropdown {
 
 		ChromeDriver driver = new ChromeDriver();
 
+		driver.get("https://www.sugarcrm.com/au/request-demo/");
+		driver.manage().window().maximize();
+		WebElement dropdown = driver.findElement(By.name("employees_c"));
+		Select select = new Select(dropdown);
+		
+		WebElement firstOption = select.getFirstSelectedOption();
+		System.out.println(firstOption.getText());
+		
+		select.selectByValue("level1");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) { // TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		select.selectByVisibleText("51 - 100 employees");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) { // TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		select.selectByIndex(5);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) { // TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+//		Part 2
+
 		/*
-		 * driver.get("https://www.sugarcrm.com/au/request-demo/");
+		 * driver.get(
+		 * "https://www.tutorialspoint.com/selenium/selenium_automation_practice.htm");
 		 * driver.manage().window().maximize(); WebElement dropdown =
-		 * driver.findElement(By.name("employees_c")); Select select = new
+		 * driver.findElement(By.name("selenium_commands")); Select select = new
 		 * Select(dropdown);
 		 */
 
@@ -32,64 +64,31 @@ public class SelectDropdown {
 		 * select.selectByValue("level1"); try { Thread.sleep(2000); } catch
 		 * (InterruptedException e) { // TODO Auto-generated catch block
 		 * e.printStackTrace(); }
-		 * 
-		 * select.selectByVisibleText("51 - 100 employees"); try { Thread.sleep(2000); }
-		 * catch (InterruptedException e) { // TODO Auto-generated catch block
-		 * e.printStackTrace(); }
-		 * 
-		 * select.selectByIndex(5); try { Thread.sleep(2000); } catch
-		 * (InterruptedException e) { // TODO Auto-generated catch block
-		 * e.printStackTrace(); }
 		 */
-
-//		Part 2
-
-		driver.get("https://www.tutorialspoint.com/selenium/selenium_automation_practice.htm");
-		driver.manage().window().maximize();
-		WebElement dropdown = driver.findElement(By.name("selenium_commands"));
-		Select select = new Select(dropdown);
 
 		/*
-		 * select.selectByValue("level1"); try { Thread.sleep(2000); } catch
+		 * select.selectByVisibleText("Navigation Commands"); try { Thread.sleep(2000);
+		 * } catch (InterruptedException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); }
+		 * 
+		 * select.selectByIndex(3); try { Thread.sleep(2000); } catch
 		 * (InterruptedException e) { // TODO Auto-generated catch block
 		 * e.printStackTrace(); }
+		 * 
+		 * List<WebElement> allItems= select.getAllSelectedOptions();
+		 * System.out.println(allItems.size()); select.deselectAll();
+		 * 
+		 * select.selectByVisibleText("Navigation Commands"); try { Thread.sleep(2000);
+		 * } catch (InterruptedException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); }
+		 * 
+		 * select.selectByIndex(3); try { Thread.sleep(2000); } catch
+		 * (InterruptedException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); }
+		 * 
+		 * select.deselectByIndex(3); List<WebElement> allItems1=
+		 * select.getAllSelectedOptions(); System.out.println(allItems1.size());
 		 */
-
-		select.selectByVisibleText("Navigation Commands");
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) { // TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		select.selectByIndex(3);
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) { // TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		List<WebElement> allItems= select.getAllSelectedOptions();
-		System.out.println(allItems.size());
-		select.deselectAll();
-		
-		select.selectByVisibleText("Navigation Commands");
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) { // TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		select.selectByIndex(3);
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) { // TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		select.deselectByIndex(3);
-		List<WebElement> allItems1= select.getAllSelectedOptions();
-		System.out.println(allItems1.size());
 
 	}
 
