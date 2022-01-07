41851ff99cbd8575efd113ff70a1c2753f0263f2
noman2016 <hafezalnoman@gmail.com>
2022-01-08 02:03:32 +0600

    updated the files

---
+++ src/test/java/ui/HandlingWindows.java
@@ -0,0 +1,38 @@
+package ui;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HandlingWindows {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();	
+		ChromeDriver driver =new ChromeDriver();	
+		driver.get("https://www.salesforce.com/au/");
+		driver.findElement(By.xpath("//*[@id=\"main\"]/div[4]/div/div/div/div[2]/div[1]/div[1]/div[3]/div/div[2]/div/div[1]/div/div[1]/div/a")).click();
+		Thread.sleep(4000);
+		Set <String> windowHandles = driver.getWindowHandles();
+		System.out.println(windowHandles);
+		
+		Iterator<String> iterator = windowHandles.iterator();
+		String parentwindow = iterator.next();
+		System.out.println(parentwindow);
+		String childwindow = iterator.next();
+		System.out.println(childwindow);
+		
+		driver.switchTo().window(childwindow);
+		
+		driver.findElement(By.name("UserFirstName")).sendKeys("AA");
+		driver.findElement(By.name("UserLastName")).sendKeys("N");
+		
+		driver.switchTo().window(parentwindow);
+		
+	}
+
+}
