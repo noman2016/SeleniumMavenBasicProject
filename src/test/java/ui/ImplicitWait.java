fda4440752f8f80d58bd51c23073b78358aa386c
noman2016 <hafezalnoman@gmail.com>
2022-01-13 03:56:57 +0600

    Add ImplicitWait file

---
+++ src/test/java/ui/ImplicitWait.java
@@ -0,0 +1,32 @@
+package ui;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ImplicitWait {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver =new ChromeDriver();
+		driver.get("https://www.ebay.com.au/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		WebElement element = driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[1]/ul/li[3]/a"));
+		Actions action = new Actions(driver);
+		action.moveToElement(element).perform();
+		
+		driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[1]/ul/li[3]/div[2]/div[1]/nav[2]/ul/li[1]/a")).click();
+		
+		Thread.sleep(5000);
+		driver.close();
+	}
+
+}
